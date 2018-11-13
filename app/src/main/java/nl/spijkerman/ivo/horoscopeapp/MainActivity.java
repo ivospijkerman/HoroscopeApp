dc1d233ffec9125db2817809fff1161e8faa5207
Ivo Spijkerman <ip.spijkerman@avans.nl>
2018-11-13 22:59:39 +0100

    initial commit

---
+++ app/src/main/java/nl/spijkerman/ivo/horoscopeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package nl.spijkerman.ivo.horoscopeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
